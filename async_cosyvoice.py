73987c628e8fcc17b8efb29968355786e9664b4a
qihua <qihua147@qq.com>
2025-03-02 16:58:49 +0800

    refactor(async_cosyvoice):优化日志输出信息
    -增加语音合成分块索引、每块耗时及总耗时等日志信息

--- async_cosyvoice.py
+++ async_cosyvoice.py
@@ -131,9 +131,13 @@ class AsyncCosyVoice2:
         for i in tqdm(self.frontend.text_normalize(tts_text, split=True, text_frontend=text_frontend)):
             model_input = self.frontend.frontend_zero_shot_by_spk_id(i, spk_id)
             start_time = time.time()
+            last_time = start_time
+            chunk_index = 0
             logging.info('synthesis text {}'.format(i))
             async for model_output in self.model.async_tts(**model_input, stream=stream, speed=speed):
                 speech_len = model_output['tts_speech'].shape[1] / self.sample_rate
-                logging.info('yield speech len {}, rtf {}'.format(speech_len, (time.time() - start_time) / speech_len))
+                logging.info('yield speech index:{}, len {:.2f}, rtf {:.3f},  cost {:.3f}s,  all cost time {:.3f}s'.format(
+                    chunk_index, speech_len,  (time.time()-last_time)/speech_len, time.time()-last_time, time.time()-start_time))
                 yield model_output
-                start_time = time.time()
+                last_time = time.time()
+                chunk_index += 1
