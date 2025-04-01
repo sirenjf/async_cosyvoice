d11f55f5a42a82d1c0e590adec3fddb7f1c628a5
qihua <qihua147@qq.com>
2025-04-01 21:53:53 +0800

    build: 提前开启 VLLM v1 模式
    - 此修改旨在确保在导入 VLLM 前，环境变量 VLLM_USE_V1 以启用 VLLM v1 版本

--- async_cosyvoice.py
+++ async_cosyvoice.py
@@ -14,6 +14,7 @@
 import os
 import time
 from typing import Generator, Union, AsyncGenerator
+os.environ["VLLM_USE_V1"] = '1'
 
 import torch
 from async_cosyvoice.frontend import CosyVoiceFrontEnd
